a4b01cfdd00b8b332d30eb693ecab95e361c583c
Helder Avila <helderavila2016@gmail.com>
2021-08-27 13:46:40 -0300

    Logout automático

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
-import { parseCookies, setCookie } from "nookies";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContext";
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -79,7 +80,10 @@ api.interceptors.response.use(
         });
       } else {
         // deslogar o usuario
+        signOut();
       }
     }
+
+    return Promise.reject(error);
   }
 );
