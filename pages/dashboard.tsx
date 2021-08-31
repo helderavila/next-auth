509b69bb2851904de06560815876d2a8ef9c0adb
Helder Avila <helderavila2016@gmail.com>
2021-08-31 13:16:15 -0300

    Hook de permissão

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,19 +1,27 @@
-import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { userCan } from "../hooks/userCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
-import { AuthTokenError } from "../services/errors/AuthTokenError"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = userCan({
+    permissions: ['metrics.list']
+  })
+
   useEffect(() => {
     api.get('/me').then(response => console.log(response)).catch(err => console.log(err))
   }, [])
 
-  return <h1>Dashboard {user?.email}</h1>
+  return (
+    <>
+      <h1>Dashboard {user?.email}</h1>
+      {userCanSeeMetrics && <div>Metrics </div>}
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
